282ae73392fc603757887ef1af92186ebb2bee2b
gabrielgc2003 <gabrielgarciaacouto@gmail.com>
2025-12-31 16:30:31 -0300

    commit inicial v1

---
+++ minhasecretariavirtual/src/main/resources/db/migration/V2__seed_tenant.sql
@@ -0,0 +1,8 @@
+INSERT INTO tenant (id, name, business_type, agenda_enabled, created_at
+VALUES (
+           '11111111-1111-1111-1111-111111111111',
+           'Cliente Teste',
+           'INFORMATIONAL',
+           false,
+           NOW()
+       );
